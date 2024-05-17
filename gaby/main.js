c6adea6dd72167a03ae9cc0890f5046b8124cf18
BALTAZARFANTINEL <103582778+BALTAZARFANTINEL@users.noreply.github.com>
2024-05-17 16:47:31 -0300

    Add files via upload

--- gaby/main.js
+++ gaby/main.js
@@ -13,6 +13,27 @@ for(let i=0;i <botoes.length;i++){
 }
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
+const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
+const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
+
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
+
+function atualizaCronometro(){
+   for (let i = 0; i < contadores.length; i++) {
+      contadores[0].textContent = calculaTempo(tempos[i]);
+   }
+}
+function comecaCronometro(){
+   atualizaCronometro();
+   setInterval(atualizaCronometro(), 1000);
+}
+comecaCronometro();
+
+contadores[1].textContent = calculaTempo(tempos[1]);
+contadores[2].textContent = calculaTempo(tempos[2]);
+contadores[3].textContent = calculaTempo(tempos[3]);
+
 let tempoAtual = new Date();
  
 contadores [0].textContent = calculaTempo(tempoObjetivo1);
@@ -26,10 +47,14 @@ function calculaTempo(tempoObjetivo1) {
    let minutos = Math.floor (segundos / 60);
    let horas = Math.floor (minutos / 60);
    let dias = Math.floor (horas / 24);
+}
 
    segundos %= 60;
    minutos %= 60;
    horas %= 24;
-return dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos";
+if (tempoFinal > 0){
+   return dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos";
+} else { 
+   return "Prazo Finalizado";
 }
 
